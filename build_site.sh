b720d344905a4c0d26495a8bd776e5f3fb4d128a
7dJx1qP <38586902+7dJx1qP@users.noreply.github.com>
2024-02-09 18:47:37 -0500

    update build script

--- build_site.sh
+++ build_site.sh
@@ -37,7 +37,8 @@ buildPlugin()
     zipfile=$(realpath "$outdir/$plugin_id.zip")
     
     pushd "$dir" > /dev/null
-    zip -r "$zipfile" . > /dev/null
+    find . -type f -exec touch -d "$updated" {} +
+    zip -0 -r -oX "$zipfile" . > /dev/null
     popd > /dev/null
 
     name=$(grep "^name:" "$f" | head -n 1 | cut -d' ' -f2- | sed -e 's/\r//' -e 's/^"\(.*\)"$/\1/')
