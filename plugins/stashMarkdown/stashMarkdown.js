a7fa46949c297a022dc12ed2beeee11ea23349d1
7dJx1qP <38586902+7dJx1qP@users.noreply.github.com>
2024-02-10 14:04:10 -0500

    add markdown settings to toggle on/off parsing for each content type

--- plugins/stashMarkdown/stashMarkdown.js
+++ plugins/stashMarkdown/stashMarkdown.js
@@ -16,63 +16,85 @@
         el.innerHTML = marked.parse(el.innerHTML);
     }
 
-    stash.addEventListener('page:tag:any', function () {
-        waitForElementByXpath("//span[contains(@class, 'detail-item-value') and contains(@class, 'description')]", function (xpath, el) {
-            el.style.display = 'block';
-            el.style.whiteSpace = 'initial';
-            processMarkdown(el);
-        });
+    let settings = null;
+    async function isMarkdownEnabled(page) {
+        if (settings === null) {
+            settings = await stash.getPluginConfig('stashMarkdown');
+        }
+        return settings?.[page];
+    }
+
+    stash.addEventListener('page:tag:any', async function () {
+        if (await isMarkdownEnabled('tags')) {
+            waitForElementByXpath("//span[contains(@class, 'detail-item-value') and contains(@class, 'description')]", function (xpath, el) {
+                el.style.display = 'block';
+                el.style.whiteSpace = 'initial';
+                processMarkdown(el);
+            });
+        }
     });
 
-    stash.addEventListener('page:tags', function () {
-        waitForElementByXpath("//div[contains(@class, 'tag-description')]", function (xpath, el) {
-            for (const node of document.querySelectorAll('.tag-description')) {
-                node.style.whiteSpace = 'initial';
-                processMarkdown(node);
-            }
-        });
+    stash.addEventListener('page:tags', async function () {
+        if (await isMarkdownEnabled('tags')) {
+            waitForElementByXpath("//div[contains(@class, 'tag-description')]", function (xpath, el) {
+                for (const node of document.querySelectorAll('.tag-description')) {
+                    node.style.whiteSpace = 'initial';
+                    processMarkdown(node);
+                }
+            });
+        }
     });
 
-    stash.addEventListener('page:scene', function () {
-        waitForElementByXpath("//div[@class='tab-content']//h6[contains(text(),'Details')]/following-sibling::p", function (xpath, el) {
-            el.style.display = 'block';
-            el.style.whiteSpace = 'initial';
-            processMarkdown(el);
-        });
+    stash.addEventListener('page:scene', async function () {
+        if (await isMarkdownEnabled('scenes')) {
+            waitForElementByXpath("//div[@class='tab-content']//h6[contains(text(),'Details')]/following-sibling::p", function (xpath, el) {
+                el.style.display = 'block';
+                el.style.whiteSpace = 'initial';
+                processMarkdown(el);
+            });
+        }
     });
 
-    stash.addEventListener('page:gallery', function () {
-        waitForElementByXpath("//div[@class='tab-content']//h6[contains(text(),'Details')]/following-sibling::p", function (xpath, el) {
-            el.style.display = 'block';
-            el.style.whiteSpace = 'initial';
-            processMarkdown(el);
-        });
+    stash.addEventListener('page:gallery', async function () {
+        if (await isMarkdownEnabled('galleries')) {
+            waitForElementByXpath("//div[@class='tab-content']//h6[contains(text(),'Details')]/following-sibling::p", function (xpath, el) {
+                el.style.display = 'block';
+                el.style.whiteSpace = 'initial';
+                processMarkdown(el);
+            });
+        }
     });
 
-    stash.addEventListener('page:movie:scenes', function () {
-        waitForElementByXpath("//span[contains(@class, 'detail-item-value') and contains(@class, 'synopsis')]", function (xpath, el) {
-            el.style.display = 'block';
-            el.style.whiteSpace = 'initial';
-            processMarkdown(el);
-        });
+    stash.addEventListener('page:movie:scenes', async function () {
+        if (await isMarkdownEnabled('movies')) {
+            waitForElementByXpath("//span[contains(@class, 'detail-item-value') and contains(@class, 'synopsis')]", function (xpath, el) {
+                el.style.display = 'block';
+                el.style.whiteSpace = 'initial';
+                processMarkdown(el);
+            });
+        }
     });
 
-    stash.addEventListener('page:movies', function () {
-        waitForElementByXpath("//div[contains(@class, 'movie-card__description')]", function (xpath, el) {
-            for (const node of document.querySelectorAll('.movie-card__description')) {
-                node.style.whiteSpace = 'initial';
-                processMarkdown(node);
-            }
-        });
+    stash.addEventListener('page:movies', async function () {
+        if (await isMarkdownEnabled('movies')) {
+            waitForElementByXpath("//div[contains(@class, 'movie-card__description')]", function (xpath, el) {
+                for (const node of document.querySelectorAll('.movie-card__description')) {
+                    node.style.whiteSpace = 'initial';
+                    processMarkdown(node);
+                }
+            });
+        }
     });
 
     // Performer details is visible regardless of chosen tab
-    function performerPageHandler() {
-        waitForElementByXpath("//span[contains(@class, 'detail-item-value') and contains(@class, 'details')]", function (xpath, el) {
-            el.style.display = 'block';
-            el.style.whiteSpace = 'initial';
-            processMarkdown(el);
-        });
+    async function performerPageHandler() {
+        if (await isMarkdownEnabled('performers')) {
+            waitForElementByXpath("//span[contains(@class, 'detail-item-value') and contains(@class, 'details')]", function (xpath, el) {
+                el.style.display = 'block';
+                el.style.whiteSpace = 'initial';
+                processMarkdown(el);
+            });
+        }
     }
     stash.addEventListener('page:performer:scenes', performerPageHandler);
     stash.addEventListener('page:performer:galleries', performerPageHandler);
@@ -81,12 +103,14 @@
     stash.addEventListener('page:performer:details', performerPageHandler);
 
     // Studio details is visible regardless of chosen tab
-    function studioPageHandler() {
-        waitForElementByXpath("//span[contains(@class, 'detail-item-value') and contains(@class, 'details')]", function (xpath, el) {
-            el.style.display = 'block';
-            el.style.whiteSpace = 'initial';
-            processMarkdown(el);
-        });
+    async function studioPageHandler() {
+        if (await isMarkdownEnabled('studios')) {
+            waitForElementByXpath("//span[contains(@class, 'detail-item-value') and contains(@class, 'details')]", function (xpath, el) {
+                el.style.display = 'block';
+                el.style.whiteSpace = 'initial';
+                processMarkdown(el);
+            });
+        }
     }
     stash.addEventListener('page:studio:galleries', studioPageHandler);
     stash.addEventListener('page:studio:images', studioPageHandler);
