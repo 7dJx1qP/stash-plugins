57c52cc7b98b92b811bf5f842b677e051dd84f1a
7dJx1qP <38586902+7dJx1qP@users.noreply.github.com>
2024-02-10 13:33:21 -0500

    add markdown parsing to scene, gallery, movie, performer, and studio details

--- plugins/stashMarkdown/stashMarkdown.js
+++ plugins/stashMarkdown/stashMarkdown.js
@@ -27,8 +27,72 @@
     stash.addEventListener('page:tags', function () {
         waitForElementByXpath("//div[contains(@class, 'tag-description')]", function (xpath, el) {
             for (const node of document.querySelectorAll('.tag-description')) {
+                node.style.whiteSpace = 'initial';
                 processMarkdown(node);
             }
         });
     });
+
+    stash.addEventListener('page:scene', function () {
+        waitForElementByXpath("//div[@class='tab-content']//h6[contains(text(),'Details')]/following-sibling::p", function (xpath, el) {
+            el.style.display = 'block';
+            el.style.whiteSpace = 'initial';
+            processMarkdown(el);
+        });
+    });
+
+    stash.addEventListener('page:gallery', function () {
+        waitForElementByXpath("//div[@class='tab-content']//h6[contains(text(),'Details')]/following-sibling::p", function (xpath, el) {
+            el.style.display = 'block';
+            el.style.whiteSpace = 'initial';
+            processMarkdown(el);
+        });
+    });
+
+    stash.addEventListener('page:movie:scenes', function () {
+        waitForElementByXpath("//span[contains(@class, 'detail-item-value') and contains(@class, 'synopsis')]", function (xpath, el) {
+            el.style.display = 'block';
+            el.style.whiteSpace = 'initial';
+            processMarkdown(el);
+        });
+    });
+
+    stash.addEventListener('page:movies', function () {
+        waitForElementByXpath("//div[contains(@class, 'movie-card__description')]", function (xpath, el) {
+            for (const node of document.querySelectorAll('.movie-card__description')) {
+                node.style.whiteSpace = 'initial';
+                processMarkdown(node);
+            }
+        });
+    });
+
+    // Performer details is visible regardless of chosen tab
+    function performerPageHandler() {
+        waitForElementByXpath("//span[contains(@class, 'detail-item-value') and contains(@class, 'details')]", function (xpath, el) {
+            el.style.display = 'block';
+            el.style.whiteSpace = 'initial';
+            processMarkdown(el);
+        });
+    }
+    stash.addEventListener('page:performer:scenes', performerPageHandler);
+    stash.addEventListener('page:performer:galleries', performerPageHandler);
+    stash.addEventListener('page:performer:movies', performerPageHandler);
+    stash.addEventListener('page:performer:appearswith', performerPageHandler);
+    stash.addEventListener('page:performer:details', performerPageHandler);
+
+    // Studio details is visible regardless of chosen tab
+    function studioPageHandler() {
+        waitForElementByXpath("//span[contains(@class, 'detail-item-value') and contains(@class, 'details')]", function (xpath, el) {
+            el.style.display = 'block';
+            el.style.whiteSpace = 'initial';
+            processMarkdown(el);
+        });
+    }
+    stash.addEventListener('page:studio:galleries', studioPageHandler);
+    stash.addEventListener('page:studio:images', studioPageHandler);
+    stash.addEventListener('page:studio:performers', studioPageHandler);
+    stash.addEventListener('page:studio:movies', studioPageHandler);
+    stash.addEventListener('page:studio:childstudios', studioPageHandler);
+    stash.addEventListener('page:studio:scenes', studioPageHandler);
+    stash.addEventListener('page:studio', studioPageHandler);
 })();
